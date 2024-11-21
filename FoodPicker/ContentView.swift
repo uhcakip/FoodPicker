67ff1065e1d9b1887b9636f9c536d3c93eea7d36
yunac <yunac.owob@gmail.com>
2024-11-21 17:02:57 +0800

    Initial Commit

---
+++ FoodPicker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FoodPicker
+//
+//  Created by Yuna Chou on 2024/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FoodPicker/FoodPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodPickerApp.swift
+//  FoodPicker
+//
+//  Created by Yuna Chou on 2024/11/21.
+//
+
+import SwiftUI
+
+@main
+struct FoodPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
